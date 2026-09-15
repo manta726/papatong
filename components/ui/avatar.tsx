33a94697ed693243e86fd45a97118284130f7121
manta726 <sabenihfermanta@gmail.com>
2026-09-15 10:05:59 +0700

    Update avatar.tsx

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -1,8 +1,8 @@
+// components/ui/avatar.tsx
 'use client';
 
 import * as React from 'react';
 import * as AvatarPrimitive from '@radix-ui/react-avatar';
-
 import { cn } from '@/lib/utils';
 
 const Avatar = React.forwardRef<
